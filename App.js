3f3561c5e9bbb99fb0c7600271d480867e828e14
Fernando <you@example.com>
2022-09-30 10:33:35 +0200

    Feature: Modifiqué el estilo de headerContainer para colocarlo en el <View> de su componente y conseguir la vizualización deseada además de crear la carpeta con la imagen y colocarla con su respectivo estilo

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from "./components/Header";
 
 /**
  * Usar git y ir generando la documentación en el Readme
+ * 
+ * No accede al componente de Header
  */
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.headerContainer}>
+        <Header />
+      </View>
+
+      <View style={styles.biographyContainer}>
+        
+      </View>
+
+      <View style={styles.qrContainer}>
+
+      </View>
+
     </View>
   );
 }
@@ -17,8 +30,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerContainer: {
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%'
+  },
+  biographyContainer: {
+    flex: 2
+  },
+  qrContainer: {
+    flex: 1
+  }
 });
+
+export default App;
